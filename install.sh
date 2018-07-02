9466d84ef64e818c2d55d785b7d4fdeb010bb720
Tobi <tobias-eberle@web.de>
2018-07-02 14:52:21 +0200

    removed sudo from install script

--- install.sh
+++ install.sh
@@ -35,26 +35,26 @@ else
 fi
 
 if [ "$flash" = true ]; then
-    sudo sh -c 'echo "deb http://archive.canonical.com/ubuntu/ trusty partner" >> /etc/apt/sources.list.d/canonical_partner.list'
+    sh -c 'echo "deb http://archive.canonical.com/ubuntu/ trusty partner" >> /etc/apt/sources.list.d/canonical_partner.list'
 fi
-sudo apt-get update
+apt-get update
 
-sudo apt-get install -y firefox htop git python-dev libxml2-dev libxslt-dev libffi-dev libssl-dev build-essential xvfb libboost-python-dev libleveldb-dev libjpeg-dev curl wget
+apt-get install -y firefox htop git python-dev libxml2-dev libxslt-dev libffi-dev libssl-dev build-essential xvfb libboost-python-dev libleveldb-dev libjpeg-dev curl wget
 
 # For some versions of ubuntu, the package libleveldb1v5 isn't available. Use libleveldb1 instead.
-sudo apt-get install -y libleveldb1v5 || sudo apt-get install -y libleveldb1
+apt-get install -y libleveldb1v5 || apt-get install -y libleveldb1
 
 if [ "$flash" = true ]; then
-    sudo apt-get install -y adobe-flashplugin
+    apt-get install -y adobe-flashplugin
 fi
 
 # Check if we're running on continuous integration
 # Python requirements are already installed by .travis.yml on Travis
 if [ "$TRAVIS" != "true" ]; then
 	wget https://bootstrap.pypa.io/get-pip.py
-	sudo -H python get-pip.py
+	-H python get-pip.py
 	rm get-pip.py
-	sudo pip install -U -r requirements.txt
+	pip install -U -r requirements.txt
 fi
 
 # Grab the latest version of Firefox ESR.
