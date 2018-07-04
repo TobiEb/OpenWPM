6dd3bf3ae651fa5fc6c43c2465330395229e3e12
Tobi <tobias-eberle@web.de>
2018-07-04 22:42:23 +0200

    add init to params of get()

--- crawl.py
+++ crawl.py
@@ -49,6 +49,7 @@ manager = TaskManager.TaskManager(manager_params, browser_params)
 
 # Visits the sites with all browsers simultaneously
 for site in sites:
+    sub_sites = []
     # define crawl actions
     # define prior login
     #command_sequence_google = CommandSequence.CommandSequence('http://accounts.google.de', reset=False)
