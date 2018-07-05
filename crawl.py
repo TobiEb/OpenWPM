df06b1161bc4dd1bc62a303f7f12a2cf985dcd87
Tobi <tobias-eberle@web.de>
2018-07-05 18:47:07 +0200

    fixed sub_sites

--- crawl.py
+++ crawl.py
@@ -45,7 +45,6 @@ manager_params['log_directory'] = '/home/OpenWPM/Output'
 default_timeout = 60
 default_sleep = 5
 subsites = SubSites()
-sub_sites = subsites.sub_sites
 
 manager = TaskManager.TaskManager(manager_params, browser_params)
 
@@ -64,27 +63,27 @@ for site in sites:
     command_sequence_get1.dump_profile_cookies(timeout=default_timeout)
     command_sequence_get1.dump_flash_cookies(timeout=default_timeout)
 
-    tests = subsites.sub_sites
-    print tests
+    print "not it is in crawl.py"
+    print subsites.sub_sites
     command_sequence_get2 = CommandSequence.CommandSequence(subsites.sub_sites[0], reset=False)
     command_sequence_get2.get(initiator=False, sleep=default_sleep, timeout=default_timeout)
     #command_sequence_get2.stop_tshark(timeout=10)
     command_sequence_get2.dump_profile_cookies(timeout=default_timeout)
     command_sequence_get2.dump_flash_cookies(timeout=default_timeout)
 
-    command_sequence_get3 = CommandSequence.CommandSequence(tests[1], reset=False)
+    command_sequence_get3 = CommandSequence.CommandSequence(subsites.sub_sites[1], reset=False)
     command_sequence_get3.get(initiator=False, sleep=default_sleep, timeout=default_timeout)
     #command_sequence_get3.stop_tshark(timeout=10)
     command_sequence_get3.dump_profile_cookies(timeout=default_timeout)
     command_sequence_get3.dump_flash_cookies(timeout=default_timeout)
 
-    command_sequence_get4 = CommandSequence.CommandSequence(tests[2], reset=False)
+    command_sequence_get4 = CommandSequence.CommandSequence(subsites.sub_sites[2], reset=False)
     command_sequence_get4.get(initiator=False, sleep=default_sleep, timeout=default_timeout)
     #command_sequence_get4.stop_tshark(timeout=10)
     command_sequence_get4.dump_profile_cookies(timeout=default_timeout)
     command_sequence_get4.dump_flash_cookies(timeout=default_timeout)
 
-    command_sequence_get5 = CommandSequence.CommandSequence(tests[3], reset=True)
+    command_sequence_get5 = CommandSequence.CommandSequence(subsites.sub_sites[3], reset=True)
     command_sequence_get5.get(initiator=False, sleep=default_sleep, timeout=default_timeout)
     #command_sequence_get5.stop_tshark(timeout=10)
     command_sequence_get5.dump_profile_cookies(timeout=default_timeout)
